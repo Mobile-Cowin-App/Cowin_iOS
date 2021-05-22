14c753b38aa463db244a784e3d7559f21b980537
Aravindhan Natarajan <>
2021-05-22 21:35:57 +0530

    - Updates

--- App/Cowin/Utilities/CWViewUtils.swift
+++ App/Cowin/Utilities/CWViewUtils.swift
@@ -15,3 +15,29 @@ internal extension UIViewController {
         return controller
     }
 }
+
+extension UIView {
+    public class func fromNib(named: String? = nil) -> Self {
+        let name = named ?? "\(Self.self)"
+        guard let nib = Bundle.main.loadNibNamed(name, owner: nil, options: nil)
+        else { fatalError("missing expected nib named: \(name)") }
+        guard
+            /// we're using `first` here because compact map chokes compiler on
+            /// optimized release, so you can't use two views in one nib if you wanted to
+            /// and are now looking at this
+            let view = nib.first as? Self
+        else { fatalError("view of type \(Self.self) not found in \(nib)") }
+        return view
+    }
+    
+    
+    internal func addShadowView(width:CGFloat=0.2, height:CGFloat=0.2, Opacidade:Float=0.7, maskToBounds:Bool=false, radius:CGFloat=0.5){
+        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.shadowOffset = CGSize(width: width, height: height)
+        self.layer.shadowRadius = radius
+        self.layer.shadowOpacity = Opacidade
+        self.layer.masksToBounds = maskToBounds
+    }
+    
+    
+}
